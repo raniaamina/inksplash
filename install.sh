aa64f769b45098a52f7d060fba194558af011ad0
Rania Amina <reaamina@gmail.com>
2017-03-31 13:55:40 +0000

    fix install and uninstall

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ echo "Installing dependencies"
 sudo apt install -y git inkscape python3 wmctrl
 echo "copying files"
 sudo git clone https://github.com/raniaamina/inksplash.git /opt/inksplash
-sudo sed -i "s/Exec=inkscape/Exec=python3 \/opt\/inksplash\/inksplash.py/" /usr/share/applications/inkscape.desktop
+sudo sed -i "s/Exec=inkscape %F/Exec=python3 \/opt\/inksplash\/inksplash.py/" /usr/share/applications/inkscape.desktop
 echo "done"
 exit
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Unistall Inksplash"
-sudo sed -i "s/Exec=python3 \/opt\/inksplash\/inksplash.py/Exec=inkscape/" /usr/share/applications/inkscape.desktop
+sudo sed -i "s/Exec=python3 \/opt\/inksplash\/inksplash.py/Exec=inkscape %F/" /usr/share/applications/inkscape.desktop
 sudo rm -rf /opt/inksplash
